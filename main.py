2be6c3377dd60753815695483439a20bd9aebaac
Neon <840654613@qq.com>
2026-01-22 10:05:53 +0800

    最终版

--- main.py
+++ main.py
@@ -4,12 +4,42 @@ from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-# 尝试导入 tags
-try:
-    from tags import IDENTITY_MAP
-except ImportError:
-    logger.warning("未找到 tags.py，成分查询功能将无法匹配标签！")
-    IDENTITY_MAP = {}
+IDENTITY_MAP = {
+
+    # 二游
+    "原神": "米友",
+    "崩坏3第一偶像爱酱": "米友",
+    "崩坏星穹铁道": "米友",
+    "星穹铁道": "米友",
+    "绝区零": "米友",
+    "明日方舟": "皱皮",
+    "鸣潮": "潮友",
+    # --- MOBA ---
+    "王者荣耀": "农友",
+    "英雄联盟": "撸狗",
+    # --- FPS ---
+    "CSGO国服": "GO学长",
+    "古堡龙姬": "GO学长",
+    "无畏契约": "瓦学弟",
+    # --- V-TUBERS: THE VIRTUAL CIRCUS ---
+    "嘉然今天吃什么": "嘉心糖",
+    "向晚大魔王": "顶碗人",
+    "乃琳Queen": "奶淇淋",
+    "贝拉kira": "贝极星",
+    "A-SOUL_Official": "一个魂",
+    "珈乐Carol": "已风车",
+    "七海Nana7mi": "脆鲨",
+    "阿梓": "小孩梓",
+    "東雪蓮Official": "罕见",
+    "永雏塔菲": "雏草姬",
+    "明前奶绿": "奶糖花",
+    # --- TECH & LIFESTYLE ---
+    "华为终端": "华粉",
+    "小米公司": "米粉",
+    # --- ABSTRACT & MEME CULTURE ---
+    "孙笑川258": "恋尸癖",
+    "理塘丁真": "理塘王"
+}
 
 
 @register("astrbot_plugin_ccf", "Neon", "B站成分查询插件", "1.2")
